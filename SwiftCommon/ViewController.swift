b5c2fa34203b9a96cdb32d841395485988ae7e3a
jiangzhenjie <zhenjie_jiang@foxmail.com>
2015-11-06 11:15:22 +0800

    initial swift common project

---
+++ SwiftCommon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCommon
+//
+//  Created by jiangzhenjie on 15/11/6.
+//  Copyright © 2015年 jiangzhenjie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
